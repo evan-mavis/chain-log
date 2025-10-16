122c4b1e65f5c338c8a6b2a6e758e315b1b32318
evan mavis <evanmavis3@gmail.com>
2025-10-16 10:49:57 -0600

    Style header

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -1,9 +1,14 @@
+import { Link } from "lucide-react";
 import UserPreferenceControls from "../user-preference-controls/UserPreferenceControls";
 
 export default function AppHeader() {
   return (
     <div className="flex w-full items-center justify-between">
-      <h1 className="m-6 text-2xl font-bold">Chain Log</h1>
+      <h1 className="m-6 flex items-center text-2xl font-bold">
+        <span>Chain</span>
+        <Link className="mx-1 text-red-500" />
+        <span>Log</span>
+      </h1>
       <UserPreferenceControls />
     </div>
   );
