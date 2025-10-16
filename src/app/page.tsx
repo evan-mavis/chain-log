8a05fa2d65076eb2fbdf743237159b79cbe7b442
evan mavis <evanmavis3@gmail.com>
2025-10-15 18:22:05 -0600

    Enhance project setup with Prettier for code formatting and add Calendar component to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Calendar } from "@/components/ui/calendar";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <h1>Hello World</h1>
+      <Calendar mode="single" className="rounded-lg border" />
     </div>
   );
 }
