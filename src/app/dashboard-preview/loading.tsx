3941504ba55f9cfb5d241b5d82a0ad4d6fe45c94
evan mavis <evanmavis3@gmail.com>
2025-10-23 14:29:51 -0400

    Add loading component to preview dashboard

---
+++ src/app/dashboard-preview/loading.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "@/components/ui/spinner";
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Spinner />
+      <p>Loading Dashboard Preview...</p>
+    </div>
+  );
+}
