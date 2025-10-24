f72b38680ae7f30764daa323d2b7e200785999c2
evan mavis <evanmavis3@gmail.com>
2025-10-24 19:07:16 -0400

    Enhance database configuration by integrating application and authentication schemas into drizzle setup.

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,10 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import * as appSchema from "@/db/schemas/schema";
+import * as authSchema from "@/db/schemas/auth-schema";
 
 const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+export const db = drizzle({
+  client: sql,
+  schema: { ...appSchema, ...authSchema },
+});
