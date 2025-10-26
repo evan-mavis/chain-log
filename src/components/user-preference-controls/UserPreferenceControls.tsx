a93a3436fe336b35c7b576dfad0872acc4e2fac9
evan mavis <evanmavis3@gmail.com>
2025-10-26 13:36:39 -0400

    Minor formatting

--- src/components/user-preference-controls/UserPreferenceControls.tsx
+++ src/components/user-preference-controls/UserPreferenceControls.tsx
@@ -1,14 +1,12 @@
 import ThemeToggle from "../theme-toggle/ThemeToggle";
 import AppAvatar from "../app-avatar/AppAvatar";
-import MessagesToggle from "../messages-toggle/MessagedToggle";
+import MessagesToggle from "../messages-toggle/MessagesToggle";
 
 export default function UserPreferenceControls() {
   return (
     <div className="mr-6 flex items-center justify-center space-x-2 rounded-md border-2 border-red-700 p-2">
       <MessagesToggle />
-
       <ThemeToggle />
-
       <AppAvatar />
     </div>
   );
